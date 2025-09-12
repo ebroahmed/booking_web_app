2f703b0dacfd8f599e7ece1dd19fb9b7232f72ac
Ebrahim <ebrahimahmed804853@gmail.com>
2025-09-12 13:50:18 +0300

    listing card updated

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'screens/home_screen.dart';
+import 'models/listing.dart';
+import 'screens/listing_detail_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -21,6 +23,16 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       home: const HomeScreen(),
+      onGenerateRoute: (settings) {
+        if (settings.name == '/listing') {
+          final listing = settings.arguments;
+          if (listing is! Listing) return null;
+          return MaterialPageRoute(
+            builder: (_) => ListingDetailScreen(listing: listing),
+          );
+        }
+        return null;
+      },
     );
   }
 }
